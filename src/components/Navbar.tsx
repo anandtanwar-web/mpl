3e2662fadab484887e8feb5e0dfab0b75addbd26
Vandana Rathore <test123@Mac-2.lan>
2026-06-11 20:14:17 +0100

    feat: integrate title sponsor logo into navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,6 +31,9 @@ const Navbar: React.FC = () => {
     <nav className="navbar">
       <div className="container nav-container">
         <div className="logo" onClick={handleHomeClick}>
+          <a href="https://guidefs.co.uk" target="_blank" rel="noopener noreferrer">
+             <img src="/guide-financials-logo.png" alt="Guide Financial Services" className="sponsor-logo-nav" />
+          </a>
           <img src="/sanskruti-logo.jpg" alt="Sanskruti Logo" className="sanskruti-logo" />
           MPL<span className="dot">.</span>
           <img src="/mpl-logo.png" alt="MPL Logo" className="mpl-logo-nav" />
